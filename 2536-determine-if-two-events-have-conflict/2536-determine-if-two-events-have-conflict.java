e5981ebab6ef33409e3644743a7352aa0068c41a
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-11-12 16:10:29 +0530

    Time: 0 ms (100%), Space: 40.8 MB (79.95%) - LeetHub

---
+++ 2536-determine-if-two-events-have-conflict/2536-determine-if-two-events-have-conflict.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        String s1=event1[0];
+        String s2=event2[0];
+        String e1=event1[1];
+        String e2=event2[1];
+
+        return s2.compareTo(e1)<=0 && s1.compareTo(e2)<=0;
+
+        
+        
+    }
+}
