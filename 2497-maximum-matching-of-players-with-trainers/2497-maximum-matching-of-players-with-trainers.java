b33dda3bb2081ed3704b3d731acb5a634932b958
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-11-12 10:46:35 +0530

    Time: 25 ms (97.72%), Space: 65.7 MB (17.93%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int l=0;
+        int r=0;
+        while(l<players.length && r< trainers.length){
+            if(players[l]<=trainers[r]){
+                l++;
+            }
+            r++;
+        }
+        return l;
+        
+    }
+}
