f8e24880729e7526cd7de07b70d5596f5bd54bb5
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-11-12 15:36:20 +0530

    Time: 45 ms (94.58%), Space: 73.1 MB (94.43%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int count =1;
+        int n=intervals.length;
+        Arrays.sort(intervals, new cmp());
+        int dead=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]>=dead){
+                count++;
+                dead=intervals[i][1];
+            }
+
+        }
+        return n-count;
+
+        
+    }
+    static class interval{
+        int start;
+        int end;
+        interval(int x, int y){
+            this.start=x;
+            this.end=y;
+        }
+    }
+    static class cmp implements Comparator<int[]>{
+        public int compare(int[] a , int[] b){
+            return a[1]-b[1];
+        }
+    }
+}
