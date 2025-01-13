0e94faa19b7176a2683d310b428e473957aa90e7
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-01-13 22:12:20 +0530

    Time: 0 ms (100%), Space: 44.9 MB (14.38%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        // if(root==null)return root;
+        // Queue<Node> qu = new LinkedList<Node>();
+        // Node main=root;
+        // qu.offer(root);
+        // while(!qu.isEmpty()){
+        //     int n=qu.size();
+        //     Node prev=null;
+        //     for(int i=0;i<n;i++){
+        //         Node curr=qu.poll();
+        //         if(prev!=null){
+        //             prev.next=curr;
+        //         }
+        //         prev=curr;
+        //         if(curr.left!=null)qu.offer(curr.left);
+        //         if(curr.right!=null)qu.offer(curr.right);
+
+                
+        //     }
+        //     prev.next=null;
+
+        // }
+        // return root;
+
+        // USING DFS NOW ------------------->>>>>>>>
+        if(root==null)return root;
+        if(root.left!=null){
+            root.left.next=root.right;
+            if(root.next!=null){
+                root.right.next=root.next.left;
+            }
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+
+    }
+}
