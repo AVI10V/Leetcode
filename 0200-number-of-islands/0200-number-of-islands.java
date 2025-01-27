56211dc0665dbaa3c4cbc738d05486b23022b554
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-01-27 22:08:30 +0530

    Time: 3 ms (86.8%), Space: 49.9 MB (34.94%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid==null || grid.length==0)return 0;
+        int ans =0;
+        int r=grid.length;
+        int c=grid[0].length;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        return ans; 
+    }
+    public void dfs(int r, int c, char[][] grid){
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]=='0')return;
+
+        grid[r][c]='0';
+        dfs(r-1,c,grid);
+        dfs(r+1,c,grid);
+        dfs(r,c-1,grid);
+        dfs(r,c+1,grid);
+
+    }
+}
