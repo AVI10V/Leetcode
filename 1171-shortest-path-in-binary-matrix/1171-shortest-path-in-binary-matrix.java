172679d33399654e217cd56616c65f5d74826fd4
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-01-28 22:37:54 +0530

    Time: 15 ms (67.96%), Space: 45.2 MB (91.33%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -0,0 +1,45 @@
+class pair{
+    int row;
+    int col;
+    int path;
+    pair(int r, int c , int l){
+        this.row=r;
+        this.col=c;
+        this.path=l;
+    }
+}
+
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int r=grid.length;
+        int c=grid[0].length;
+        if(grid.length==1 && grid[0][0]==0)return 1;
+        int[][] vis = new int[r][c];
+        int len =0;
+        Queue<pair> qu = new LinkedList<pair>();
+        qu.offer(new pair(0,0,1));
+        if(grid[0][0]==1)return -1;
+        vis[0][0]=1;
+        int[] drow={0,-1,-1,-1,0,1,1,1};
+        int[] dcol={-1,-1,0,1,1,1,0,-1};
+        while(!qu.isEmpty()){
+            int row=qu.peek().row;
+            int col=qu.peek().col;
+            int ll=qu.peek().path;
+            qu.remove();
+            for(int i=0;i<8;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<c && vis[nrow][ncol]!=1 && grid[nrow][ncol]==0){
+                    if(nrow==r-1 && ncol==c-1)return ll+1;
+                    qu.offer(new pair(nrow, ncol,ll+1));
+                    vis[nrow][ncol]=1;
+                }
+            }
+
+        }
+    return -1;
+
+        
+    }
+}
