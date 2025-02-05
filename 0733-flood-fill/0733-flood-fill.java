84874d5260e766bffd4b3b78ca5e9f8f6d5b85c4
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-02-05 23:11:29 +0530

    Time: 0 ms (100%), Space: 45 MB (48.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -2,19 +2,19 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int row=image.length;
         int col=image[0].length;
-        int orignalcolor=image[sr][sc];
-        if(orignalcolor!=color){
-            dfs(image,sr,sc,color,orignalcolor);
+        int orignal=image[sr][sc];
+        if(orignal!=color){
+            dfs(image,sr,sc,color,orignal);
         }
-                
         return image;
+        
     }
-    public void dfs(int[][] image, int r, int c , int color , int oc){
-        if(r<0 || r>=image.length|| c<0 || c>=image[0].length|| image[r][c]!=oc)return;
+    public void dfs(int[][] image,int r, int c, int color, int oc){
+        if(r<0 || r>=image.length|| c<0 || c>=image[0].length || image[r][c]!=oc)return;
         image[r][c]=color;
         dfs(image,r-1,c,color,oc);
         dfs(image,r+1,c,color,oc);
-        dfs(image,r,c-1,color,oc);
         dfs(image,r,c+1,color,oc);
+        dfs(image,r,c-1,color,oc);
     }
 }
