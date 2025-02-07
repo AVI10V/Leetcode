5528f38614ccd91c76a0e9478e961b4e02cb3f3e
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-02-07 13:52:17 +0530

    Time: 4 ms (51.05%), Space: 48.6 MB (99.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -10,20 +10,49 @@ class Solution {
             for(int j=0;j<c;j++){
                 if(grid[i][j] == '1'){
                     ans++;
-                    dfs(i,j,grid);
+                    bfs(new pair(i,j),grid);
                 }
             }
         }
         return ans; 
     }
-    public void dfs(int r, int c, char[][] grid){
-        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]=='0')return;
-
+    class pair{
+        int row;
+        int col;
+        public pair(int x, int y){
+            this.row=x;
+            this.col=y;
+        }
+    }
+    public boolean isvalid(int r, int c, char[][] mat){
+        if(r<0|| r>=mat.length|| c<0 || c>=mat[0].length )return false;
+        return true;
+    }
+    public void bfs(pair p , char[][] grid){
+        Queue<pair> qu = new LinkedList<pair>();
+        qu.offer(p);
+        int r=p.row;
+        int c=p.col;
         grid[r][c]='0';
-        dfs(r-1,c,grid);
-        dfs(r+1,c,grid);
-        dfs(r,c-1,grid);
-        dfs(r,c+1,grid);
+        int[] drow={0,-1,0,+1};
+        int[] dcol={-1,0,+1,0};
+        while(!qu.isEmpty()){
+            pair pp= qu.poll();
+            int curr=pp.row;
+            int curc=pp.col;
+            for(int i=0;i<4;i++){
+                int nrow=curr+drow[i];
+                int ncol=curc+dcol[i];
+                if(isvalid(nrow,ncol,grid) && grid[nrow][ncol]=='1'){
+                    grid[nrow][ncol]='0';
+                    qu.offer(new pair(nrow,ncol));
+
+                }
+            }
+
+        }
+        
+        
 
     }
 }
