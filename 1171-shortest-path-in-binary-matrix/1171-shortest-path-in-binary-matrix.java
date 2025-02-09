7029a00ae4909b52983880c281f8accb7d929a3c
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-02-09 15:47:29 +0530

    Time: 12 ms (94.86%), Space: 45.3 MB (84.82%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -14,12 +14,10 @@ class Solution {
         int r=grid.length;
         int c=grid[0].length;
         if(grid.length==1 && grid[0][0]==0)return 1;
-        int[][] vis = new int[r][c];
         int len =0;
         Queue<pair> qu = new LinkedList<pair>();
         qu.offer(new pair(0,0,1));
         if(grid[0][0]==1)return -1;
-        vis[0][0]=1;
         int[] drow={0,-1,-1,-1,0,1,1,1};
         int[] dcol={-1,-1,0,1,1,1,0,-1};
         while(!qu.isEmpty()){
@@ -30,10 +28,11 @@ class Solution {
             for(int i=0;i<8;i++){
                 int nrow=row+drow[i];
                 int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<c && vis[nrow][ncol]!=1 && grid[nrow][ncol]==0){
+                if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<c  && grid[nrow][ncol]==0){
                     if(nrow==r-1 && ncol==c-1)return ll+1;
+                    grid[nrow][ncol]=1;
                     qu.offer(new pair(nrow, ncol,ll+1));
-                    vis[nrow][ncol]=1;
+                    
                 }
             }
 
