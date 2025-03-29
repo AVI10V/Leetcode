e03e292f427da7ca914f7e20e578d7e190bc6f07
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-03-29 08:09:55 +0530

    Time: 1 ms (82.82%), Space: 41.7 MB (71.44%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,24 +1,24 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String ans ="";
+       StringBuilder ans = new StringBuilder();
       int s1=0;
       int s2=0;
       int n1=word1.length();
       int n2=word2.length();
       while(s1<n1 && s2<n2){
-          ans+=word1.charAt(s1);
+          ans.append(word1.charAt(s1));
           s1++;
-          ans+=word2.charAt(s2);
+         ans.append(word2.charAt(s2));
           s2++;
       }  
       while(s1<n1){
-        ans+=word1.charAt(s1);
+        ans.append(word1.charAt(s1));
         s1++;
       }
       while(s2<n2){
-        ans+=word2.charAt(s2);
+        ans.append(word2.charAt(s2));
         s2++;
       }
-      return ans;
+      return ans.toString();
     }
 }
