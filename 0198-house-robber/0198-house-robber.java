ff057b33a76966a805f070f13e59328f0c10411d
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-04-27 11:43:10 +0530

    Time: 0 ms (100%), Space: 41 MB (76.88%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        dp[0]=nums[0];
+        int prev=nums[0];
+        int prev2=0;
+        int curr=0;
         for(int i=1;i<n;i++){
-            int pick=nums[i];
-            int dont=dp[i-1];
-            if(i>1){
-                pick+=dp[i-2];
-            }
-            dp[i]=Math.max(pick,dont);
+            int pick=nums[i]+prev2;
+            int dont=prev;
+            curr=Math.max(pick,dont);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
     }
 }
