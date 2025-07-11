bf2f64e19c62f5356786a529f0e22a3f4e313fe8
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-07-11 14:36:14 +0530

    Time: 856 ms (10.62%), Space: 52.4 MB (24.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,15 +1,14 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int target=0;
-        Arrays.sort(nums);
-        Set<List<Integer>> set= new HashSet<>();
+        Set<List<Integer>> set = new HashSet<>();
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
-            int j =i+1;
+            int j=i+1;
             int k=nums.length-1;
             while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
+                int sum=nums[i]+nums[j]+nums[k];
                 if(sum==target){
                     set.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
@@ -21,11 +20,10 @@ class Solution {
                 else{
                     k--;
                 }
-
             }
         }
         ans.addAll(set);
         return ans;
-        
+
     }
 }
