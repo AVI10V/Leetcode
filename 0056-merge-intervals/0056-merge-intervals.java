a5cd2e451683a4b5eb9397beef6ceeea364551f3
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-07-11 16:15:42 +0530

    Time: 7 ms (98.4%), Space: 46.7 MB (39.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> merged = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int i=0;
-       for(int[] x:intervals){
-        if(merged.isEmpty()|| merged.get(merged.size()-1)[1]<x[0]){
-            merged.add(x);
+        List<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals , (a,b)->Integer.compare(a[0] , b[0]));
+        for(int[] x:intervals){
+            int n=result.size()-1;
+           if(result.isEmpty() || result.get(n)[1]<x[0]){
+            result.add(x);
+           }
+           else{
+            result.get(n)[1]=Math.max(x[1] , result.get(n)[1]);
+           }
         }
-        else{
-            merged.get(merged.size()-1)[1]=Math.max(merged.get(merged.size()-1)[1],x[1]);
-        }
-       }
-        return merged.toArray(new int[merged.size()][]);
+        return result.toArray(new int[result.size()][]);
     }
 }
