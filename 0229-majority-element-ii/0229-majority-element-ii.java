d4e487df1efc7c8462e6a843e90f0e63129d6a4c
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-07-12 11:05:06 +0530

    Time: 3 ms (75.99%), Space: 50.5 MB (41.76%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
+        Integer e1=null;
+        Integer e2=null;
+        int count1 =0;
+        int count2 =0;
         int n=nums.length;
-        Map<Integer , Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+           if(e1!=null && nums[i]==e1){
+            count1++;
+           }
+           else if(e2!=null && nums[i]==e2){
+            count2++;
+           }
+           else if(count1==0){
+            e1=nums[i];
+            count1=1;
+           }
+           else if(count2==0){
+            e2=nums[i];
+            count2=1;
+           }
+           else{
+            count1--;
+            count2--;
+           }
+        }
+        count1=0;
+        count2=0;
         for(int x:nums){
-            map.put(x , map.getOrDefault(x , 0)+1);
+            if(x==e1)count1++;
+            else if(x==e2)count2++;
         }
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            if(entry.getValue()>(n/3)){
-                ans.add(entry.getKey());
-            }
-        }
+        List<Integer> ans = new ArrayList<>();
+        if(count1>n/3)ans.add(e1);
+        if(count2>n/3)ans.add(e2);
         return ans;
+
     }
 }
