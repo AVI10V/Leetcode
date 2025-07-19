c319e592d70ecd99abf4750015d5fb79aa27cf3e
AVI10V <119165939+AVI10V@users.noreply.github.com>
2025-07-20 00:17:30 +0530

    Time: 0 ms (100%), Space: 42.1 MB (7.29%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxDepth(String s) {
-        int n= s.length();
-        Stack<Character> st = new Stack<>();
-        int max =0;
+        int n=s.length();
+        int max=0;
+        int balance=0;
         for(int i=0;i<n;i++){
-            if(s.charAt(i)=='('){
-                st.push('(');
-            }
-            else if(s.charAt(i)==')'){
-                st.pop();
-            }
-            max=Math.max(max,st.size());
-
+           if(s.charAt(i)=='('){
+            balance++;
+           }
+           else if(s.charAt(i)==')'){
+            balance--;
+           }
+           max=Math.max(max , balance);
         }
         return max;
-
+        
     }
 }
